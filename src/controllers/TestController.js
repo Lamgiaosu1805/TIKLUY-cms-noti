57e89499ed7ed9957cab0791dd14fc44614d847f
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-09-17 08:25:35 +0700

    improve

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -8,47 +8,70 @@ const TestController = {
     },
     pushNotification: async (req, res, next) => {
         const body = req.body
-        const expo = new Expo({accessToken: process.env.EXPO_ACCESSTOKEN, useFcmV1: true})
-        const message = {
-            to: "ExponentPushToken[TdUSBLFvm6y1ZVPKxHMf5b]", // Replace with a valid push token from your app
-            title: "CMS Tikluy Thông báo",
-            body: "Khách hàng LÂM ĐZAI đã tạo lệnh rút 100.000.000 VNĐ",
-            data: { someData: 'Some data to be sent with the notification' }, // Optional data
-        };
-        // const message2 = {
-        //     to: "ExponentPushToken[TdUSBLFvm6y1ZVPKxHMf5b]", // Replace with a valid push token from your app
-        //     title: 'My Notification Titleeeeee',
-        //     body: 'This is the notification body content',
-        //     data: { someData: 'Some data to be sent with the notification' }, // Optional data
-        // };
-        (async () => {
-        try {
-            const { tickets, errors } = await expo.sendPushNotificationsAsync([message]);
-                // handlePushTicketResponses(tickets);
-                // handleErrorResponses(errors);
-            } catch (error) {
-                console.error('Error sending notifications:', error);
+        let expo = new Expo({
+            accessToken: process.env.EXPO_ACCESS_TOKEN,
+            /*
+             * @deprecated
+             * The optional useFcmV1 parameter defaults to true, as FCMv1 is now the default for the Expo push service.
+             *
+             * If using FCMv1, the useFcmV1 parameter may be omitted.
+             * Set this to false to have Expo send to the legacy endpoint.
+             *
+             * See https://firebase.google.com/support/faq#deprecated-api-shutdown
+             * for important information on the legacy endpoint shutdown.
+             *
+             * Once the legacy service is fully shut down, the parameter will be removed in a future PR.
+             */
+            useFcmV1: true,
+        });
+          
+        // Create the messages that you want to send to clients
+        let messages = [];
+        const somePushTokens = ["ExponentPushToken[B68CZkDcReDFFW6mGqXanb]"]
+        for (let pushToken of somePushTokens) {
+            // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+            
+            // Check that all your push tokens appear to be valid Expo push tokens
+            if (!Expo.isExpoPushToken(pushToken)) {
+                console.error(`Push token ${pushToken} is not a valid Expo push token`);
+                continue;
             }
-        })();
-        res.send("a")
+            
+            // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+            messages.push({
+                to: pushToken,
+                sound: 'default',
+                body: body.body,
+                data: { withSome: 'data' },
+            })
+        }
         
-        function handlePushTicketResponses(tickets) {
-        // Process tickets for successful and failed deliveries
-            for (const ticket of tickets) {
-                const { status, error } = ticket;
-                console.log(`Ticket ${ticket.id}: ${status}`);
-                if (error) {
-                console.error(`Error (${error.code}): ${error.message}`);
+        // The Expo push notification service accepts batches of notifications so
+        // that you don't need to send 1000 requests to send 1000 notifications. We
+        // recommend you batch your notifications to reduce the number of requests
+        // and to compress them (notifications with similar content will get
+        // compressed).
+        let chunks = expo.chunkPushNotifications(messages);
+        let tickets = [];
+        (async () => {
+        // Send the chunks to the Expo push notification service. There are
+        // different strategies you could use. A simple one is to send one chunk at a
+        // time, which nicely spreads the load out over time:
+            for (let chunk of chunks) {
+                try {
+                    let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                    console.log(ticketChunk);
+                    tickets.push(...ticketChunk);
+                    // NOTE: If a ticket contains an error code in ticket.details.error, you
+                    // must handle it appropriately. The error codes are listed in the Expo
+                    // documentation:
+                    // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                } catch (error) {
+                    console.error(error);
                 }
             }
-        }
-        
-        function handleErrorResponses(errors) {
-            // Handle errors related to the Expo push notification service
-            for (const error of errors) {
-                console.error(`Expo server error: ${error}`);
-            }
-        }
+        })();
+        res.send('a')
     }
 }
 
